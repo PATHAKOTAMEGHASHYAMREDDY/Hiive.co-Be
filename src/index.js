577f85e3754d4e9fefff11e8ef0d168f2f063e2b
shyam <meghashyamreddy014@gmail.com>
2025-08-24 16:12:16 +0530

    added fe url in cors

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://hiiveco.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
